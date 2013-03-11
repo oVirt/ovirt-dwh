875e4ffa9b9eddeb7c461cac9070b7f262c853aa
Yaniv Dary <ydary@redhat.com>
2013-03-11 14:54:05 +0200

    history: added utility fuction to update enum values (#918075)
    Bug-Url: https://bugzilla.redhat.com/918075
    
    Change-Id: I5921a20165466cba5f5ab7870dafb3d53cda8b8d
    Signed-off-by: Yaniv Dary <ydary@redhat.com>

---
+++ data-warehouse/historydbscripts_postgres/upgrade/03_02_0050_add_function_to_update_enum_table.sql
@@ -0,0 +1,14 @@
+CREATE OR REPLACE FUNCTION update_enum_translator(enum_type varchar(40), enum_key smallint, language_code varchar(40), value text) RETURNS void AS $$
+BEGIN
+    DELETE FROM enum_translator
+    WHERE enum_translator.enum_type = update_enum_translator.enum_type
+          AND enum_translator.enum_key = update_enum_translator.enum_key
+          AND enum_translator.language_code = update_enum_translator.language_code
+          AND EXISTS (SELECT 1
+                      FROM enum_translator as a
+                      WHERE a.enum_type = update_enum_translator.enum_type
+                            AND a.enum_key = update_enum_translator.enum_key
+                            AND a.language_code = update_enum_translator.language_code);
+    INSERT INTO enum_translator VALUES (update_enum_translator.enum_type, update_enum_translator.enum_key, update_enum_translator.language_code, update_enum_translator.value);
+END;
+$$ LANGUAGE plpgsql;
