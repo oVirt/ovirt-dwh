5ed4322babea884c8438dad337b141d0265dca8f
Yaniv Dary <ydary@redhat.com>
2013-05-26 15:10:41 +0300

    history: added missing enum keys for OS (#966437)
    Bug-Url: https://bugzilla.redhat.com/966437
    
    Change-Id: I86f8c98a54b686c470939c2601a355c7e3abc0f6
    Signed-off-by: Yaniv Dary <ydary@redhat.com>

---
+++ data-warehouse/historydbscripts_postgres/upgrade/03_02_0080_added_missing_os_enums.sql
@@ -0,0 +1,26 @@
+--add windows 8
+SELECT update_enum_translator(cast('OS_TYPE' as varchar(40)),cast(20 as smallint),cast('de' as varchar(40)),cast('Windows 8' as text));
+SELECT update_enum_translator(cast('OS_TYPE' as varchar(40)),cast(20 as smallint),cast('en_US' as varchar(40)),cast('Windows 8' as text));
+SELECT update_enum_translator(cast('OS_TYPE' as varchar(40)),cast(20 as smallint),cast('es' as varchar(40)),cast('Windows 8' as text));
+SELECT update_enum_translator(cast('OS_TYPE' as varchar(40)),cast(20 as smallint),cast('fr' as varchar(40)),cast('Windows 8' as text));
+SELECT update_enum_translator(cast('OS_TYPE' as varchar(40)),cast(20 as smallint),cast('ja' as varchar(40)),cast('Windows 8' as text));
+SELECT update_enum_translator(cast('OS_TYPE' as varchar(40)),cast(20 as smallint),cast('pt_BR' as varchar(40)),cast('Windows 8' as text));
+SELECT update_enum_translator(cast('OS_TYPE' as varchar(40)),cast(20 as smallint),cast('zh_CN' as varchar(40)),cast('Windows 8' as text));
+
+--add windows 8 x64
+SELECT update_enum_translator(cast('OS_TYPE' as varchar(40)),cast(21 as smallint),cast('de' as varchar(40)),cast('Windows 8 x64' as text));
+SELECT update_enum_translator(cast('OS_TYPE' as varchar(40)),cast(21 as smallint),cast('en_US' as varchar(40)),cast('Windows 8 x64' as text));
+SELECT update_enum_translator(cast('OS_TYPE' as varchar(40)),cast(21 as smallint),cast('es' as varchar(40)),cast('Windows 8 x64' as text));
+SELECT update_enum_translator(cast('OS_TYPE' as varchar(40)),cast(21 as smallint),cast('fr' as varchar(40)),cast('Windows 8 x64' as text));
+SELECT update_enum_translator(cast('OS_TYPE' as varchar(40)),cast(21 as smallint),cast('ja' as varchar(40)),cast('Windows 8 x64' as text));
+SELECT update_enum_translator(cast('OS_TYPE' as varchar(40)),cast(21 as smallint),cast('pt_BR' as varchar(40)),cast('Windows 8 x64' as text));
+SELECT update_enum_translator(cast('OS_TYPE' as varchar(40)),cast(21 as smallint),cast('zh_CN' as varchar(40)),cast('Windows 8 x64' as text));
+
+--add windows 2012
+SELECT update_enum_translator(cast('OS_TYPE' as varchar(40)),cast(23 as smallint),cast('de' as varchar(40)),cast('Windows 2012 x64' as text));
+SELECT update_enum_translator(cast('OS_TYPE' as varchar(40)),cast(23 as smallint),cast('en_US' as varchar(40)),cast('Windows 2012 x64' as text));
+SELECT update_enum_translator(cast('OS_TYPE' as varchar(40)),cast(23 as smallint),cast('es' as varchar(40)),cast('Windows 2012 x64' as text));
+SELECT update_enum_translator(cast('OS_TYPE' as varchar(40)),cast(23 as smallint),cast('fr' as varchar(40)),cast('Windows 2012 x64' as text));
+SELECT update_enum_translator(cast('OS_TYPE' as varchar(40)),cast(23 as smallint),cast('ja' as varchar(40)),cast('Windows 2012 x64' as text));
+SELECT update_enum_translator(cast('OS_TYPE' as varchar(40)),cast(23 as smallint),cast('pt_BR' as varchar(40)),cast('Windows 2012 x64' as text));
+SELECT update_enum_translator(cast('OS_TYPE' as varchar(40)),cast(23 as smallint),cast('zh_CN' as varchar(40)),cast('Windows 2012 x64' as text));
