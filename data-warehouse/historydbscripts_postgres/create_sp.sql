5ce767244a77fbb491a563a5fe67662a36b9f64e
Yaniv Dary <ydary@redhat.com>
2013-06-11 16:38:04 +0300

    history: fixed issue with enum strings insertion (#952418)
    insertion now compatible with postgres 8.x.
    
    Bug-Url: https://bugzilla.redhat.com/952418
    
    Change-Id: If9f0f6ef8560aea0af6bb300018203dabbd09230
    Signed-off-by: Yaniv Dary <ydary@redhat.com>

--- data-warehouse/historydbscripts_postgres/create_sp.sql
+++ data-warehouse/historydbscripts_postgres/create_sp.sql
@@ -1,1 +1,15 @@
 --This file is a place holder for the creation of stored procedures in the oVirt Engine History database.
+CREATE OR REPLACE FUNCTION update_enum_translator(enum_type varchar(40), enum_key smallint, language_code varchar(40), value text) RETURNS void AS $$
+BEGIN
+    DELETE FROM enum_translator
+    WHERE enum_translator.enum_type = update_enum_translator.enum_type
+          AND enum_translator.enum_key = update_enum_translator.enum_key
+          AND enum_translator.language_code = update_enum_translator.language_code
+          AND EXISTS (SELECT 1
+                      FROM enum_translator as a
+                      WHERE a.enum_type = update_enum_translator.enum_type
+                            AND a.enum_key = update_enum_translator.enum_key
+                            AND a.language_code = update_enum_translator.language_code);
+    INSERT INTO enum_translator VALUES (update_enum_translator.enum_type, update_enum_translator.enum_key, update_enum_translator.language_code, update_enum_translator.value);
+END;
+$$ LANGUAGE plpgsql;
