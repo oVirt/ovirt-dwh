b41930873c97824cf6301c2040421149aaeaea4b
Alex Lourie <alourie@redhat.com>
2013-10-15 12:58:24 +0200

    history-etl: service: update discovery of JAVA_HOME
    The new implementation uses engine's script
    /usr/share/ovirt-engine/bin/java-home to detect JAVA_HOME
    location.
    
    Change-Id: I34dd5891cfc76242bb3bb50bf77bb48ad50839c8
    Bug-Url: https://bugzilla.redhat.com/1019178
    Related-To: https://bugzilla.redhat.com/1009863
    Signed-off-by: Alex Lourie <alourie@redhat.com>

--- data-warehouse/history_etl/history_service/history_service.sh
+++ data-warehouse/history_etl/history_service/history_service.sh
@@ -10,6 +10,7 @@ done
 LOGFILE="/var/log/ovirt-engine/ovirt-engine-dwhd.log"
 ETL_HOME=/usr/share/ovirt-engine-dwh/etl
 JAVA_DIR=/usr/share/java
+JAVA_HOME=$(/usr/share/ovirt-engine/bin/java-home)
 RUN_PROPERTIES="-Xms256M -Xmx1024M"
 CP=$ETL_HOME:$JAVA_DIR/ovirt-engine-dwh/historyETLProcedure.jar:$JAVA_DIR/ovirt-engine-dwh/advancedPersistentLookupLib.jar:$JAVA_DIR/ovirt-engine-dwh/talendRoutines.jar:$JAVA_DIR/dom4j.jar:$JAVA_DIR/commons-collections.jar:$JAVA_DIR/log4j.jar:$JAVA_DIR/postgresql-jdbc.jar
 
