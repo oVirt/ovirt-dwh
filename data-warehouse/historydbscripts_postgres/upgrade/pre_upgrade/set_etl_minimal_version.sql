444b6f4ed51757d749868e58bbaa2f2bc37f054d
Sandro Bonazzola <sbonazzo@redhat.com>
2014-02-25 09:50:45 +0100

    build: post ovirt-engine-dwh-3.3.4
    Change-Id: I8385c95737a8faf259dd996a7cfad25b9d812c8b
    Signed-off-by: Sandro Bonazzola <sbonazzo@redhat.com>

--- data-warehouse/historydbscripts_postgres/upgrade/pre_upgrade/set_etl_minimal_version.sql
+++ data-warehouse/historydbscripts_postgres/upgrade/pre_upgrade/set_etl_minimal_version.sql
@@ -1,2 +1,2 @@
-INSERT INTO history_configuration(var_name,var_value) SELECT 'MinimalETLVersion','3.3.4' WHERE not exists (SELECT var_name FROM history_configuration WHERE var_name = 'MinimalETLVersion');
-UPDATE history_configuration SET var_value = '3.3.4' WHERE var_name = 'MinimalETLVersion';
+INSERT INTO history_configuration(var_name,var_value) SELECT 'MinimalETLVersion','3.3.5' WHERE not exists (SELECT var_name FROM history_configuration WHERE var_name = 'MinimalETLVersion');
+UPDATE history_configuration SET var_value = '3.3.5' WHERE var_name = 'MinimalETLVersion';
