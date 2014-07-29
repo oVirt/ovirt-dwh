ab1dd6c238fe319ce4a26a6a085a08cdb5dcdcf9
Shirly Radco <sradco@redhat.com>
2014-07-29 14:39:16 +0300

    packaging: updated insert_calendar_table_values script
    fixed the script so that the values generated
    will not be affected due to timezones that are not
    of round hours.
    
    Change-Id: Ib525d093d6c54f3153f23f7d499362ba6d4fb25b
    Signed-off-by: Shirly Radco <sradco@redhat.com>

---
+++ packaging/dbscripts/upgrade/03_04_0100_updated_insert_calendar_table_values_script.sql
@@ -0,0 +1,15 @@
+-- Update insert_calendar_table_values script to disregard timezones
+TRUNCATE TABLE calendar;
+INSERT INTO calendar( the_datetime, the_date, the_year, the_month, month_name, the_day, day_name, the_hour )
+SELECT
+     date_time as the_datetime,
+     date_trunc('day', date_time) as the_date,
+     extract (year from date_time) as the_year,
+     extract (month from date_time) as the_month,
+     initcap(to_char(date_time, 'FMmonth')) as month_name,
+     extract (day from date_time) as the_day,
+     initcap(to_char(date_time, 'FMday')) as day_name,
+     cast(date_time as time) as the_hour
+FROM
+   (SELECT generate_series('2011-01-01 00:00'::timestamp, '2021-01-01 00:00', '1 hours') AS date_time) AS hour_list
+ORDER BY date_time;
