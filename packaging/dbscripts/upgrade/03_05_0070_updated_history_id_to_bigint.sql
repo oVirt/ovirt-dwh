48a1af4ab16ec548702cd2622c6071eb60974816
Shirly Radco <sradco@redhat.com>
2014-09-15 16:03:03 +0300

    history: Update statistics table history_id to bigint
    Update host_interface statistics tables
    history_id to bigint.
    
    Change-Id: I355e47354ca23899141aa2b0adc570105cbc02ae
    Bug-Url: https://bugzilla.redhat.com/1121994
    Signed-off-by: Shirly Radco <sradco@redhat.com>
    (cherry picked from commit ae2625830b290b13357f7abb29da51a83b587bcf)

--- packaging/dbscripts/upgrade/03_05_0070_updated_history_id_to_bigint.sql
+++ packaging/dbscripts/upgrade/03_05_0070_updated_history_id_to_bigint.sql
@@ -6,9 +6,9 @@ SELECT fn_db_change_column_type('datacenter_samples_history', 'history_id', 'int
 SELECT fn_db_change_column_type('host_daily_history', 'history_id', 'integer', 'bigint');
 SELECT fn_db_change_column_type('host_hourly_history', 'history_id', 'integer', 'bigint');
 SELECT fn_db_change_column_type('host_samples_history', 'history_id', 'integer', 'bigint');
-SELECT fn_db_change_column_type('host_integererface_daily_history', 'history_id', 'integer', 'bigint');
-SELECT fn_db_change_column_type('host_integererface_hourly_history', 'history_id', 'integer', 'bigint');
-SELECT fn_db_change_column_type('host_integererface_samples_history', 'history_id', 'integer', 'bigint');
+SELECT fn_db_change_column_type('host_interface_daily_history', 'history_id', 'integer', 'bigint');
+SELECT fn_db_change_column_type('host_interface_hourly_history', 'history_id', 'integer', 'bigint');
+SELECT fn_db_change_column_type('host_interface_samples_history', 'history_id', 'integer', 'bigint');
 SELECT fn_db_change_column_type('statistics_vms_users_usage_daily', 'history_id', 'integer', 'bigint');
 SELECT fn_db_change_column_type('statistics_vms_users_usage_hourly', 'history_id', 'integer', 'bigint');
 SELECT fn_db_change_column_type('storage_domain_daily_history', 'history_id', 'integer', 'bigint');
