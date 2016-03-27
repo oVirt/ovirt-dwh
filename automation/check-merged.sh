b1648249470f10d2eab47f49a75ead375913a076
Shirly Radco <sradco@redhat.com>
2016-03-27 12:26:35 +0300

    packaging: add automation for standard ci
    Added automation directory for standard ci.
    
    Change-Id: I698c9ccf0d32afe8cc3418533b290a1f9fc2d920
    Signed-off-by: Shirly Radco <sradco@redhat.com>

---
+++ automation/check-merged.sh
@@ -0,0 +1,66 @@
+#!/bin/bash -xe
+
+SUFFIX=".git$(git rev-parse --short HEAD)"
+MAVEN_SETTINGS="/etc/maven/settings.xml"
+export BUILD_JAVA_OPTS_MAVEN="\
+    -Dgwt.compiler.localWorkers=1 \
+"
+
+# Set the location of the JDK that will be used for compilation:
+export JAVA_HOME="${JAVA_HOME:=/usr/lib/jvm/java-1.8.0}"
+
+# Use ovirt mirror if able, fall back to central maven
+mkdir -p "${MAVEN_SETTINGS%/*}"
+cat >"$MAVEN_SETTINGS" <<EOS
+<?xml version="1.0"?>
+<settings xmlns="http://maven.apache.org/POM/4.0.0"
+          xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+          xsi:schemaLocation="http://maven.apache.org/POM/4.0.0
+          http://maven.apache.org/xsd/settings-1.0.0.xsd">
+
+<mirrors>
+        <mirror>
+                <id>ovirt-maven-repository</id>
+                <name>oVirt artifactory proxy</name>
+                <url>http://artifactory.ovirt.org/artifactory/ovirt-mirror</url>
+                <mirrorOf>*</mirrorOf>
+        </mirror>
+        <mirror>
+                <id>root-maven-repository</id>
+                <name>Official maven repo</name>
+                <url>http://repo.maven.apache.org/maven2</url>
+                <mirrorOf>*</mirrorOf>
+        </mirror>
+</mirrors>
+</settings>
+EOS
+# remove any previous artifacts
+rm -rf output
+rm -f ./*tar.gz
+make clean
+
+# Get the tarball
+make dist
+
+# create the src.rpm
+rpmbuild \
+    -D "_srcrpmdir $PWD/output" \
+    -D "_topmdir $PWD/rpmbuild" \
+    -D "release_suffix ${SUFFIX}" \
+    -ts ./*.gz
+
+# install any build requirements
+yum-builddep output/*src.rpm
+
+if [[ "$(rpm --eval "%{dist}")" ==  ".fc22" ]]; then
+    # Needed to download the deps before compilation, for some reason it fails
+    # inside fc22 chroot if not done first
+    mvn dependency:resolve-plugins
+fi
+
+# create the rpms
+rpmbuild \
+    -D "_rpmdir $PWD/output" \
+    -D "_topmdir $PWD/rpmbuild" \
+    -D "release_suffix ${SUFFIX}" \
+    --rebuild output/*.src.rpm
