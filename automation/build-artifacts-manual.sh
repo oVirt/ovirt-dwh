b211804b98181efa966e388332ee364c5a2312e1
Sharon Naftaly <snaftaly@redhat.com>
2016-08-01 17:41:22 +0300

    Adding build-artifacts-manual.sh script
    Adding build-artifacts-manual.sh script and related repo and
    packages files.
    
    Change-Id: I8e0527e15907ceaf47b3c18edee73e893bb7fefa
    Signed-off-by: Sharon Naftaly <snaftaly@redhat.com>

---
+++ automation/build-artifacts-manual.sh
@@ -0,0 +1,56 @@
+#!/bin/bash -xe
+
+MAVEN_SETTINGS="/etc/maven/settings.xml"
+
+# Set the location of the JDK that will be used for compilation:
+export JAVA_HOME="${JAVA_HOME:=/usr/lib/jvm/java-1.8.0}"
+
+# Use ovirt mirror if able, fall back to central maven
+mkdir -p "${MAVEN_SETTINGS%/*}"
+cat >"$MAVEN_SETTINGS" <<EOS
+<?xml version="1.0"?>
+<settings xmlns="http://maven.apache.org/POM/4.0.0"
+          xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+          xsi:schemaLocation="http://maven.apache.org/POM/4.0.0
+          http://maven.apache.org/xsd/settings-1.0.0.xsd">
+
+<mirrors>
+        <mirror>
+                <id>ovirt-maven-repository</id>
+                <name>oVirt artifactory proxy</name>
+                <url>http://artifactory.ovirt.org/artifactory/ovirt-mirror</url>
+                <mirrorOf>*</mirrorOf>
+        </mirror>
+        <mirror>
+                <id>root-maven-repository</id>
+                <name>Official maven repo</name>
+                <url>http://repo.maven.apache.org/maven2</url>
+                <mirrorOf>*</mirrorOf>
+        </mirror>
+</mirrors>
+</settings>
+EOS
+# remove any previous artifacts
+rm -rf output
+make clean
+
+# create the src.rpm, assuming the tarball is in the directory
+rpmbuild \
+    -D "_srcrpmdir $PWD/output" \
+    -D "_topmdir $PWD/rpmbuild" \
+    -ts ./*.gz
+
+# install any build requirements
+yum-builddep output/*src.rpm
+
+# create the rpms
+rpmbuild \
+    -D "_rpmdir $PWD/output" \
+    -D "_topmdir $PWD/rpmbuild" \
+    --rebuild output/*.src.rpm
+
+# Store any relevant artifacts in exported-artifacts for the ci system to
+# archive
+[[ -d exported-artifacts ]] || mkdir -p exported-artifacts
+find output -iname \*rpm -exec mv "{}" exported-artifacts/ \;
+mv ./*tar.gz exported-artifacts/
