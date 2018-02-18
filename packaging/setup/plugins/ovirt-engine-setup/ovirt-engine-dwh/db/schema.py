464b3c146bc5da8dd3548185c39134c39192ca4c
Yedidyah Bar David <didi@redhat.com>
2018-02-18 10:23:43 +0200

    packaging: setup: postgres95: Fixes
    - Upgrade in main transaction
    
    Before this patch, postgres upgrade was done in its own transaction.
    So rollback could only happen if there was a failure in the upgrade
    process.
    
    Move this to otopi's MAIN_TRANSACTION, which is where most changes are
    done - so that if we fail elsewhere and rollback, we also rollback
    postgresql.
    
    - Do not backup/restore on upgrade
    
    Not needed, because we can revert to the old postgresql for rollback.
    
    Change-Id: Ieeace3bb4c784ab8090b43dafcd8150b0f461ce4
    Bug-Url: https://bugzilla.redhat.com/1546487
    Signed-off-by: Yedidyah Bar David <didi@redhat.com>

--- packaging/setup/plugins/ovirt-engine-setup/ovirt-engine-dwh/db/schema.py
+++ packaging/setup/plugins/ovirt-engine-setup/ovirt-engine-dwh/db/schema.py
@@ -200,8 +200,14 @@ class Plugin(plugin.PluginBase):
 
     @plugin.event(
         stage=plugin.Stages.STAGE_CUSTOMIZATION,
+        # If we are upgrading to a newer postgresql, do not backup.
+        # If we upgrade by copying, we can rollback by using the old
+        # version. If we upgrade in-place, we do not support rollback,
+        # and user should take care of backups elsewhere.
         condition=lambda self: not self.environment[
             odwhcons.DBEnv.NEW_DATABASE
+        ] and not self.environment[
+            odwhcons.DBEnv.NEED_DBMSUPGRADE
         ],
         before=(
             oengcommcons.Stages.DIALOG_TITLES_E_DATABASE,
