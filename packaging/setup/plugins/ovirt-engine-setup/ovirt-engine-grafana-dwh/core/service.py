385e5e369316412ccd40a75e0e3c1e5fbf68eaaf
Yedidyah Bar David <didi@redhat.com>
2020-08-26 17:13:08 +0300

    packaging: setup: Give grafana more time to start
    Temporarily set systemd's TimeoutStartSec high, so that if grafana takes
    more time to start, likely due to provisioning, it succeeds.
    
    Bug-Url: https://bugzilla.redhat.com/1861368
    Change-Id: Ic266a0ab981efe6c597c6ded83168ff248edb5ee
    Signed-off-by: Yedidyah Bar David <didi@redhat.com>

--- packaging/setup/plugins/ovirt-engine-setup/ovirt-engine-grafana-dwh/core/service.py
+++ packaging/setup/plugins/ovirt-engine-setup/ovirt-engine-grafana-dwh/core/service.py
@@ -8,10 +8,13 @@
 
 
 import gettext
+import os
 
 
 from otopi import util
 from otopi import plugin
+from otopi import filetransaction
+from otopi import transaction
 
 
 from ovirt_engine_setup import constants as osetupcons
@@ -24,6 +27,69 @@ def _(m):
 
 @util.export
 class Plugin(plugin.PluginBase):
+
+    class SystemdTempConfTransaction(transaction.TransactionElement):
+        """systemd temporary conf transaction element."""
+
+        def __init__(self, parent, service, values):
+            self._parent = parent
+            self._service = service
+            self._values = values
+            self._dir = os.path.join(
+                '/etc',
+                'systemd',
+                'system',
+                '{}.service.d'.format(service),
+            )
+            self._conf = os.path.join(
+                self._dir,
+                'engine-setup-temporary.conf',
+            )
+            self._direxisted = False
+            self._conftransaction = transaction.Transaction()
+
+        def __str__(self):
+            return _("systemd temporary conf Transaction")
+
+        def _reload_systemd_conf(self):
+            systemctl = self._parent.command.get('systemctl', optional=True)
+            if systemctl is not None:
+                self._parent.execute(
+                    args=(
+                        systemctl,
+                        'daemon-reload',
+                    ),
+                    raiseOnError=False,
+                )
+
+        def prepare(self):
+            self._direxisted = os.path.exists(self._dir)
+            if not self._direxisted:
+                os.makedirs(self._dir)
+            content = [
+                '[Service]',
+            ] + [
+                '{k}={v}'.format(k=k, v=val)
+                for k, val in self._values.items()
+            ]
+            self._conftransaction.append(
+                filetransaction.FileTransaction(
+                    name=self._conf,
+                    content=content,
+                    visibleButUnsafe=True,
+                )
+            )
+            self._conftransaction.prepare()
+            self._reload_systemd_conf()
+
+        def abort(self):
+            self._conftransaction.abort()
+            if not self._direxisted:
+                os.rmdir(self._dir)
+            self._reload_systemd_conf()
+
+        def commit(self):
+            self._conftransaction.commit()
 
     def __init__(self, context):
         super(Plugin, self).__init__(context=context)
@@ -50,10 +116,29 @@ class Plugin(plugin.PluginBase):
     )
     def _closeup_grafana_service(self):
         self.logger.info(_('Starting Grafana service'))
-        self.services.state(
-            name=ogdwhcons.Const.SERVICE_NAME,
-            state=True,
-        )
+        # If this is initial setup, grafana startup does also provisioning.
+        # This can take a rather long time on slow/loaded machines.
+        # systemd's default DefaultTimeoutStartSec is 90 seconds.
+        # Temporarily allow grafana more time.
+        localtransaction = transaction.Transaction()
+        try:
+            localtransaction.append(
+                self.SystemdTempConfTransaction(
+                    parent=self,
+                    service=ogdwhcons.Const.SERVICE_NAME,
+                    values={
+                        'TimeoutStartSec': '300',
+                    },
+                )
+            )
+            localtransaction.prepare()
+            self.services.state(
+                name=ogdwhcons.Const.SERVICE_NAME,
+                state=True,
+            )
+        finally:
+            localtransaction.abort()
+
         self.services.startup(
             name=ogdwhcons.Const.SERVICE_NAME,
             state=True,
