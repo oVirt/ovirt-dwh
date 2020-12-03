73b661e310bfa3cecf3f1002721a4ddca1b4416f
Aviv Litman <alitman@redhat.com>
2020-12-03 14:49:54 +0200

    Add types of storage and storage domain to enum_translator table
    keys and Types added:
    STORAGE_DOMAIN_TYPE:
    4	Image
    5	Volume
    6	Unknown
    7	ManagedBlockStorage
    8	Unmanaged
    
    STORAGE_TYPE:
    7	GLUSTERFS
    8	GLANCE
    9	CINDER
    10	MANAGED_BLOCK_STORAGE
    11	UNMANAGED
    
    Change-Id: I247d1233d17e5b388b63db752a5083fc2a2919e9
    Bug-Url: https://bugzilla.redhat.com/1904047
    Signed-off-by: Aviv Litman <alitman@redhat.com>

---
+++ packaging/dbscripts/upgrade/04_04_0010_add_storage_and_storage_domain_enums.sql
@@ -0,0 +1,84 @@
+SET standard_conforming_strings TO on;
+-- Updated values and keys for enum_type: STORAGE_DOMAIN_TYPE
+-- Updated value for enum_key 4
+SELECT update_enum_translator(cast('STORAGE_DOMAIN_TYPE' as varchar(40)),cast(4 as smallint),cast('de' as varchar(40)),cast('Image' as text));
+SELECT update_enum_translator(cast('STORAGE_DOMAIN_TYPE' as varchar(40)),cast(4 as smallint),cast('en_US' as varchar(40)),cast('Image' as text));
+SELECT update_enum_translator(cast('STORAGE_DOMAIN_TYPE' as varchar(40)),cast(4 as smallint),cast('es' as varchar(40)),cast('Image' as text));
+SELECT update_enum_translator(cast('STORAGE_DOMAIN_TYPE' as varchar(40)),cast(4 as smallint),cast('fr' as varchar(40)),cast('Image' as text));
+SELECT update_enum_translator(cast('STORAGE_DOMAIN_TYPE' as varchar(40)),cast(4 as smallint),cast('ja' as varchar(40)),cast('Image' as text));
+SELECT update_enum_translator(cast('STORAGE_DOMAIN_TYPE' as varchar(40)),cast(4 as smallint),cast('pt_BR' as varchar(40)),cast('Image' as text));
+SELECT update_enum_translator(cast('STORAGE_DOMAIN_TYPE' as varchar(40)),cast(4 as smallint),cast('zh_CN' as varchar(40)),cast('Image' as text));
+-- Updated value for enum_key 5
+SELECT update_enum_translator(cast('STORAGE_DOMAIN_TYPE' as varchar(40)),cast(5 as smallint),cast('de' as varchar(40)),cast('Volume' as text));
+SELECT update_enum_translator(cast('STORAGE_DOMAIN_TYPE' as varchar(40)),cast(5 as smallint),cast('en_US' as varchar(40)),cast('Volume' as text));
+SELECT update_enum_translator(cast('STORAGE_DOMAIN_TYPE' as varchar(40)),cast(5 as smallint),cast('es' as varchar(40)),cast('Volume' as text));
+SELECT update_enum_translator(cast('STORAGE_DOMAIN_TYPE' as varchar(40)),cast(5 as smallint),cast('fr' as varchar(40)),cast('Volume' as text));
+SELECT update_enum_translator(cast('STORAGE_DOMAIN_TYPE' as varchar(40)),cast(5 as smallint),cast('ja' as varchar(40)),cast('Volume' as text));
+SELECT update_enum_translator(cast('STORAGE_DOMAIN_TYPE' as varchar(40)),cast(5 as smallint),cast('pt_BR' as varchar(40)),cast('Volume' as text));
+SELECT update_enum_translator(cast('STORAGE_DOMAIN_TYPE' as varchar(40)),cast(5 as smallint),cast('zh_CN' as varchar(40)),cast('Volume' as text));
+-- Updated value for enum_key 6
+SELECT update_enum_translator(cast('STORAGE_DOMAIN_TYPE' as varchar(40)),cast(6 as smallint),cast('de' as varchar(40)),cast('Unknown' as text));
+SELECT update_enum_translator(cast('STORAGE_DOMAIN_TYPE' as varchar(40)),cast(6 as smallint),cast('en_US' as varchar(40)),cast('Unknown' as text));
+SELECT update_enum_translator(cast('STORAGE_DOMAIN_TYPE' as varchar(40)),cast(6 as smallint),cast('es' as varchar(40)),cast('Unknown' as text));
+SELECT update_enum_translator(cast('STORAGE_DOMAIN_TYPE' as varchar(40)),cast(6 as smallint),cast('fr' as varchar(40)),cast('Unknown' as text));
+SELECT update_enum_translator(cast('STORAGE_DOMAIN_TYPE' as varchar(40)),cast(6 as smallint),cast('ja' as varchar(40)),cast('Unknown' as text));
+SELECT update_enum_translator(cast('STORAGE_DOMAIN_TYPE' as varchar(40)),cast(6 as smallint),cast('pt_BR' as varchar(40)),cast('Unknown' as text));
+SELECT update_enum_translator(cast('STORAGE_DOMAIN_TYPE' as varchar(40)),cast(6 as smallint),cast('zh_CN' as varchar(40)),cast('Unknown' as text));
+-- Updated value for enum_key 7
+SELECT update_enum_translator(cast('STORAGE_DOMAIN_TYPE' as varchar(40)),cast(7 as smallint),cast('de' as varchar(40)),cast('Managed Block Storage' as text));
+SELECT update_enum_translator(cast('STORAGE_DOMAIN_TYPE' as varchar(40)),cast(7 as smallint),cast('en_US' as varchar(40)),cast('Managed Block Storage' as text));
+SELECT update_enum_translator(cast('STORAGE_DOMAIN_TYPE' as varchar(40)),cast(7 as smallint),cast('es' as varchar(40)),cast('Managed Block Storage' as text));
+SELECT update_enum_translator(cast('STORAGE_DOMAIN_TYPE' as varchar(40)),cast(7 as smallint),cast('fr' as varchar(40)),cast('Managed Block Storage' as text));
+SELECT update_enum_translator(cast('STORAGE_DOMAIN_TYPE' as varchar(40)),cast(7 as smallint),cast('ja' as varchar(40)),cast('Managed Block Storage' as text));
+SELECT update_enum_translator(cast('STORAGE_DOMAIN_TYPE' as varchar(40)),cast(7 as smallint),cast('pt_BR' as varchar(40)),cast('Managed Block Storage' as text));
+SELECT update_enum_translator(cast('STORAGE_DOMAIN_TYPE' as varchar(40)),cast(7 as smallint),cast('zh_CN' as varchar(40)),cast('Managed Block Storage' as text));
+-- Updated value for enum_key 8
+SELECT update_enum_translator(cast('STORAGE_DOMAIN_TYPE' as varchar(40)),cast(8 as smallint),cast('de' as varchar(40)),cast('Unmanaged' as text));
+SELECT update_enum_translator(cast('STORAGE_DOMAIN_TYPE' as varchar(40)),cast(8 as smallint),cast('en_US' as varchar(40)),cast('Unmanaged' as text));
+SELECT update_enum_translator(cast('STORAGE_DOMAIN_TYPE' as varchar(40)),cast(8 as smallint),cast('es' as varchar(40)),cast('Unmanaged' as text));
+SELECT update_enum_translator(cast('STORAGE_DOMAIN_TYPE' as varchar(40)),cast(8 as smallint),cast('fr' as varchar(40)),cast('Unmanaged' as text));
+SELECT update_enum_translator(cast('STORAGE_DOMAIN_TYPE' as varchar(40)),cast(8 as smallint),cast('ja' as varchar(40)),cast('Unmanaged' as text));
+SELECT update_enum_translator(cast('STORAGE_DOMAIN_TYPE' as varchar(40)),cast(8 as smallint),cast('pt_BR' as varchar(40)),cast('Unmanaged' as text));
+SELECT update_enum_translator(cast('STORAGE_DOMAIN_TYPE' as varchar(40)),cast(8 as smallint),cast('zh_CN' as varchar(40)),cast('Unmanaged' as text));
+
+-- Updated values and keys for enum_type: STORAGE_TYPE
+-- Updated value for enum_key 7
+SELECT update_enum_translator(cast('STORAGE_TYPE' as varchar(40)),cast(7 as smallint),cast('de' as varchar(40)),cast('Glusterfs' as text));
+SELECT update_enum_translator(cast('STORAGE_TYPE' as varchar(40)),cast(7 as smallint),cast('en_US' as varchar(40)),cast('Glusterfs' as text));
+SELECT update_enum_translator(cast('STORAGE_TYPE' as varchar(40)),cast(7 as smallint),cast('es' as varchar(40)),cast('Glusterfs' as text));
+SELECT update_enum_translator(cast('STORAGE_TYPE' as varchar(40)),cast(7 as smallint),cast('fr' as varchar(40)),cast('Glusterfs' as text));
+SELECT update_enum_translator(cast('STORAGE_TYPE' as varchar(40)),cast(7 as smallint),cast('ja' as varchar(40)),cast('Glusterfs' as text));
+SELECT update_enum_translator(cast('STORAGE_TYPE' as varchar(40)),cast(7 as smallint),cast('pt_BR' as varchar(40)),cast('Glusterfs' as text));
+SELECT update_enum_translator(cast('STORAGE_TYPE' as varchar(40)),cast(7 as smallint),cast('zh_CN' as varchar(40)),cast('Glusterfs' as text));
+-- Updated value for enum_key 8
+SELECT update_enum_translator(cast('STORAGE_TYPE' as varchar(40)),cast(8 as smallint),cast('de' as varchar(40)),cast('Glance' as text));
+SELECT update_enum_translator(cast('STORAGE_TYPE' as varchar(40)),cast(8 as smallint),cast('en_US' as varchar(40)),cast('Glance' as text));
+SELECT update_enum_translator(cast('STORAGE_TYPE' as varchar(40)),cast(8 as smallint),cast('es' as varchar(40)),cast('Glance' as text));
+SELECT update_enum_translator(cast('STORAGE_TYPE' as varchar(40)),cast(8 as smallint),cast('fr' as varchar(40)),cast('Glance' as text));
+SELECT update_enum_translator(cast('STORAGE_TYPE' as varchar(40)),cast(8 as smallint),cast('ja' as varchar(40)),cast('Glance' as text));
+SELECT update_enum_translator(cast('STORAGE_TYPE' as varchar(40)),cast(8 as smallint),cast('pt_BR' as varchar(40)),cast('Glance' as text));
+SELECT update_enum_translator(cast('STORAGE_TYPE' as varchar(40)),cast(8 as smallint),cast('zh_CN' as varchar(40)),cast('Glance' as text));
+-- Updated value for enum_key 9
+SELECT update_enum_translator(cast('STORAGE_TYPE' as varchar(40)),cast(9 as smallint),cast('de' as varchar(40)),cast('Cinder' as text));
+SELECT update_enum_translator(cast('STORAGE_TYPE' as varchar(40)),cast(9 as smallint),cast('en_US' as varchar(40)),cast('Cinder' as text));
+SELECT update_enum_translator(cast('STORAGE_TYPE' as varchar(40)),cast(9 as smallint),cast('es' as varchar(40)),cast('Cinder' as text));
+SELECT update_enum_translator(cast('STORAGE_TYPE' as varchar(40)),cast(9 as smallint),cast('fr' as varchar(40)),cast('Cinder' as text));
+SELECT update_enum_translator(cast('STORAGE_TYPE' as varchar(40)),cast(9 as smallint),cast('ja' as varchar(40)),cast('Cinder' as text));
+SELECT update_enum_translator(cast('STORAGE_TYPE' as varchar(40)),cast(9 as smallint),cast('pt_BR' as varchar(40)),cast('Cinder' as text));
+SELECT update_enum_translator(cast('STORAGE_TYPE' as varchar(40)),cast(9 as smallint),cast('zh_CN' as varchar(40)),cast('Cinder' as text));
+-- Updated value for enum_key 10
+SELECT update_enum_translator(cast('STORAGE_TYPE' as varchar(40)),cast(10 as smallint),cast('de' as varchar(40)),cast('Managed Block Storage' as text));
+SELECT update_enum_translator(cast('STORAGE_TYPE' as varchar(40)),cast(10 as smallint),cast('en_US' as varchar(40)),cast('Managed Block Storage' as text));
+SELECT update_enum_translator(cast('STORAGE_TYPE' as varchar(40)),cast(10 as smallint),cast('es' as varchar(40)),cast('Managed Block Storage' as text));
+SELECT update_enum_translator(cast('STORAGE_TYPE' as varchar(40)),cast(10 as smallint),cast('fr' as varchar(40)),cast('Managed Block Storage' as text));
+SELECT update_enum_translator(cast('STORAGE_TYPE' as varchar(40)),cast(10 as smallint),cast('ja' as varchar(40)),cast('Managed Block Storage' as text));
+SELECT update_enum_translator(cast('STORAGE_TYPE' as varchar(40)),cast(10 as smallint),cast('pt_BR' as varchar(40)),cast('Managed Block Storage' as text));
+SELECT update_enum_translator(cast('STORAGE_TYPE' as varchar(40)),cast(10 as smallint),cast('zh_CN' as varchar(40)),cast('Managed Block Storage' as text));
+-- Updated value for enum_key 11
+SELECT update_enum_translator(cast('STORAGE_TYPE' as varchar(40)),cast(11 as smallint),cast('de' as varchar(40)),cast('Unmanaged' as text));
+SELECT update_enum_translator(cast('STORAGE_TYPE' as varchar(40)),cast(11 as smallint),cast('en_US' as varchar(40)),cast('Unmanaged' as text));
+SELECT update_enum_translator(cast('STORAGE_TYPE' as varchar(40)),cast(11 as smallint),cast('es' as varchar(40)),cast('Unmanaged' as text));
+SELECT update_enum_translator(cast('STORAGE_TYPE' as varchar(40)),cast(11 as smallint),cast('fr' as varchar(40)),cast('Unmanaged' as text));
+SELECT update_enum_translator(cast('STORAGE_TYPE' as varchar(40)),cast(11 as smallint),cast('ja' as varchar(40)),cast('Unmanaged' as text));
+SELECT update_enum_translator(cast('STORAGE_TYPE' as varchar(40)),cast(11 as smallint),cast('pt_BR' as varchar(40)),cast('Unmanaged' as text));
+SELECT update_enum_translator(cast('STORAGE_TYPE' as varchar(40)),cast(11 as smallint),cast('zh_CN' as varchar(40)),cast('Unmanaged' as text));
