44c15de28df657a023b3c4b0e3b58f25fb58bd88
Yedidyah Bar David <didi@redhat.com>
2021-07-26 11:33:29 +0300

    packaging: setup: Usually default to configure grafana
    We used to configure grafana by default.
    
    This broke upgrade/restore of hosted-engine from 4.3 if the
    backed up engine was set up with a remote DWH database[1].
    
    To fix this, we kept the default in DWH to True, but patched
    the appliance to override this to False, unconditionally.
    This means that hosted-engine setups never configured grafana
    out-of-the-box, and always required configuring it manually
    later if needed, which is sub-optimal for most users.
    
    We want to configure grafana by default in more cases. So:
    Default to True (Configure grafana), unless all of the following
    are true:
    1. The DWH database is not new/empty
    2. The DWH database host is not 'localhost'
    3. OTOPI's "accept defaults" flag is not used ('--accept-defaults'
    in the engine-setup command line, or
    'DIALOG/autoAcceptDefault=bool:True' in the answer file)
    
    I hope this is enough to keep [1] fixed but configure grafana in most
    other cases.
    [1] https://bugzilla.redhat.com/show_bug.cgi?id=1866780
    
    Change-Id: I059f28114eb8189afc4facb01bc63bce2c83bcda
    Bug-Url: https://bugzilla.redhat.com/1980315
    Signed-off-by: Yedidyah Bar David <didi@redhat.com>

--- packaging/setup/plugins/ovirt-engine-setup/ovirt-engine-grafana-dwh/core/misc.py
+++ packaging/setup/plugins/ovirt-engine-setup/ovirt-engine-grafana-dwh/core/misc.py
@@ -10,12 +10,14 @@
 import gettext
 
 
+from otopi import constants as otopicons
 from otopi import util
 from otopi import plugin
 
 from ovirt_engine_setup.engine import constants as oenginecons
 from ovirt_engine_setup.engine_common import constants as oengcommcons
 from ovirt_engine_setup import constants as osetupcons
+from ovirt_engine_setup.dwh import constants as odwhcons
 from ovirt_engine_setup.grafana_dwh import constants as ogdwhcons
 from ovirt_setup_lib import dialog
 
@@ -30,6 +32,27 @@ class Plugin(plugin.PluginBase):
     def __init__(self, context):
         super(Plugin, self).__init__(context=context)
 
+    def _configure_grafana_default(self):
+        # We used to configure grafana by default.
+        # This broke upgrade/restore of hosted-engine from 4.3 if the
+        # backed up engine was set up with a remote DWH database[1].
+        # To fix this, we kept the default here to True, but patched
+        # the appliance to override this to False, unconditionally.
+        # This means that hosted-engine setups never configured grafana
+        # out-of-the-box, and always required configuring it manually
+        # later if needed.
+        # I am going to revert that patch to the appliance, and instead
+        # make the default here slightly more complex.
+        # [1] https://bugzilla.redhat.com/show_bug.cgi?id=1866780
+        if (
+            not self.environment[odwhcons.DBEnv.NEW_DATABASE]
+            and self.environment[odwhcons.DBEnv.HOST] != 'localhost'
+            and self.environment[otopicons.DialogEnv.AUTO_ACCEPT_DEFAULT]
+        ):
+            return False
+        return True
+
+
     @plugin.event(
         stage=plugin.Stages.STAGE_CUSTOMIZATION,
         name=ogdwhcons.Stages.CORE_ENABLE,
@@ -38,7 +61,10 @@ class Plugin(plugin.PluginBase):
         ),
         after=(
             osetupcons.Stages.DIALOG_TITLES_S_PRODUCT_OPTIONS,
-            oenginecons.Stages.CORE_ENABLE
+            # No real need to ask after asking about the engine/DWH, but it
+            # seems more sensible.
+            oenginecons.Stages.CORE_ENABLE,
+            odwhcons.Stages.CORE_ENABLE,
         ),
     )
     def _customization_enable_grafana(self):
@@ -53,7 +79,7 @@ class Plugin(plugin.PluginBase):
                     '(@VALUES@) [@DEFAULT@]: '
                 ),
                 prompt=True,
-                default=True,
+                default=self._configure_grafana_default(),
             )
         if self.environment[ogdwhcons.CoreEnv.ENABLE]:
             self.environment[oengcommcons.ApacheEnv.ENABLE] = True
